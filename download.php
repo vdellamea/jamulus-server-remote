4ec54a62fbcab6163cc57ab03153050f6ab5b75b
vdellamea <75564007+vdellamea@users.noreply.github.com>
2020-12-19 11:44:57 +0100

    Update download.php

--- download.php
+++ download.php
@@ -1,6 +1,6 @@
 <?php
 // Jamulus Server Remote
-// v0.1 - 20201206
+// v0.3 - 20201219
 // Vincenzo Della Mea
 
 // PROTECTED DOWNLOAD
@@ -8,7 +8,7 @@ session_start();
 include("config.php");
 if((isset($_SESSION['admin'])&& ($_SESSION['admin']==$ADMINPASSWORD)) ||
 	(isset($_SESSION['musician'])&& ($_SESSION['musician']==$MUSICIANPASSWORD))) {
-
+	
 	if(isset($_GET['what']) && $_GET['what']=='all') 
 		$file="$RECORDINGS"."session.zip";
 	else if(isset($_GET['what']) && $_GET['what']=='today') 
@@ -17,17 +17,15 @@ if((isset($_SESSION['admin'])&& ($_SESSION['admin']==$ADMINPASSWORD)) ||
 		die("No, thanks");
 		
 	if(file_exists($file)){
-	header('Content-Description: Download recordings');
-    	header('Content-Type: application/octet-stream');
-    	header('Content-Disposition: attachment; filename="'.basename($file).'"');
-    	header('Expires: 0');
-    	header('Cache-Control: must-revalidate');
-    	header('Pragma: public');
-    	header('Content-Length: ' . filesize($file));
-    	readfile($file);
-    	exit;	
-	}
-
-	}
-	
+		header('Content-Description: Download recordings');
+    		header('Content-Type: application/octet-stream');
+    		header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    		header('Expires: 0');
+    		header('Cache-Control: must-revalidate');
+    		header('Pragma: public');
+    		header('Content-Length: ' . filesize($file));
+    		readfile($file);
+    		exit;	
+		}
+	}	
 ?>
