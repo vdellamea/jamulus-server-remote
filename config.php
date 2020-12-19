0437a76917f166783bb82628de6bc0d97ac9e28a
vdellamea <75564007+vdellamea@users.noreply.github.com>
2020-12-19 11:38:01 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,24 +1,25 @@
-<?php
-// Jamulus Server Remote
-// v0.1 - 20201206
+<?php 
+// Jamulus Server Remote 
+// v0.3 - 20201219 
 // Vincenzo Della Mea
 
 // CONFIGURATION FILE
 
-$SERVERNAME="Sparse Band";
-$PASSWORD= "4strings";
-$RECORDINGS="/var/www/html/recording/";
-$RECURL="recording/";
-$DEBUG=false;
+$SERVERNAME="Sparse Band"; 
+$ADMINPASSWORD= "4strings"; // change it!
+$MUSICIANPASSWORD="6strings"; // change it!
+$RECORDINGS="/home/jamulus/recording/"; //the same set in jamulus.service
+$ARCHIVE="/home/jamulus/recording/"; //not yet used
+$DEBUG=false; 
 
 $today=date("Ymd");
 $COMMANDS=array(
- "toggle" => "sudo systemctl start jamulus-start-stop ",
- "newrec" => "sudo systemctl start jamulus-new ",
- "compress" => "rm session.zip; zip -r session.zip $RECORDINGS/Jam* ",
- "compressday" => "rm $today.zip; zip -r $today.zip $RECORDINGS"."Jam-$today-* ", 
+ "toggle" => "sudo /bin/systemctl kill -s SIGUSR2 jamulus ",
+ "newrec" => "sudo /bin/systemctl kill -s SIGUSR1 jamulus ",
+ "compress" => "cd $RECORDINGS ; rm session.zip; zip -r session.zip Jam* ",
+ "compressday" => "cd $RECORDINGS ; rm $today.zip; zip -r $today.zip Jam-$today-* ", 
  "cleanup" => "rm -fr $RECORDINGS/Jam* ",
- "listrec" => "du -sh $RECORDINGS/* ",
+ "listrec" => "du -sh $RECORDINGS/Jam* ",
  );
 
 
