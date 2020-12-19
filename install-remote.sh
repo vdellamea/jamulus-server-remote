96d60bd3df8d4912cf2aabb55747450687a72610
vdellamea <75564007+vdellamea@users.noreply.github.com>
2020-12-19 11:42:23 +0100

    Update install-remote.sh

--- install-remote.sh
+++ install-remote.sh
@@ -1,14 +1,14 @@
-sudo apt-get install apache2 php libapache2-mod-php zip
+sudo apt-get install apache2 php libapache2-mod-php
+
 
 sudo rm /var/www/html/index.html
 sudo cp *.php /var/www/html/
-
-sudo mkdir /var/www/html/recording
-
 sudo chown -R www-data /var/www/html/
 sudo chgrp -R www-data /var/www/html/
-sudo chmod g+rwx /var/www/html/recording/
-sudo chmod g+s /var/www/html/recording/
-sudo setfacl -d -m g::rwx /var/www/html/recording
+
+sudo mkdir /home/jamulus/recording
+sudo chmod g+rwx /home/jamulus/recording/
+sudo chmod g+s /home/jamulus/recording/
+sudo setfacl -d -m g::rwx /home/jamulus/recording/
 
 sudo cp files/jamulus-sudoers.txt /etc/sudoers.d/jamulus
